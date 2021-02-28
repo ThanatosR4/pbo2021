56c922090c95b718f07030c87e5bd08ce51e724e
Rahmat Ramadhan Al Hais <haisalhais@gmail.com>
2021-02-28 21:02:26 +0700

    Praktikum 03 - Membuat class User, MahasiswaBaru, Pegawai dan Dosen

--- index.php
+++ index.php
@@ -1,29 +1,25 @@
 <?php
- 
-include('Mahasiswa.php');
 
-$mahasiswa_Ais = new Mahasiswa();
-$mahasiswa_Yuni = new Mahasiswa();
-$mahasiswa_Faiz = new Mahasiswa();
- 
-$mahasiswa_Ais->nim = "H1101191048" ;
-$mahasiswa_Ais->nama = "Rahmat Ramadhan Al Hais" ;
-$mahasiswa_Ais->tanggal_lahir = "23-12-2000" ;
-$mahasiswa_Ais->jenis_kelamin = "laki-Laki" ;
+require_once ('Mahasiswa.php');
+$ais = new Mahasiswa("H1101191048","Rahmat Ramadhan Al Hais","2000-12-23","Laki-Laki");
+$yuni = new Mahasiswa("H1101191031","Yunida Salsabila","2001-10-16","Perempuan");
+$faiz = new Mahasiswa("H1101191034","Faiz Al Fauzi","2000-09-20","Laki-Laki");
+?>
+<html>
+ <head>
+   <title>PHP Test</title>
+ </head>
+ <body>
+   <?php echo '<p>Hello World</p>'; ?>
+   <?php
+    $ais->tampilkanNama();
+   ?><br>
+   <?php
+    $ais->tampilkanUmur();
+   ?><br>
+   <?php
+    $ais->tampilkanAngkatan();
+   ?><br>
 
-$mahasiswa_Yuni->nim = "H1101191031";
-$mahasiswa_Yuni->nama = "Yunida Salsabila" ;
-$mahasiswa_Yuni->tanggal_lahir = "16-10-2001";
-$mahasiswa_Yuni->jenis_kelamin = "Perempuan";
-
-$mahasiswa_Faiz->nim = "H1101191034";
-$mahasiswa_Faiz->nama = "Faiz Al Fauzi";
-$mahasiswa_Faiz->tanggal_lahir = "20-09-2000" ;
-$mahasiswa_Faiz->jenis_kelamin = "Laki-Laki";
-
-$mahasiswa_Ais->tampilkanAngkatan();
-$mahasiswa_Ais->tampilkanUmur()  ;
-$mahasiswa_Yuni->tampilkanAngkatan();
-$mahasiswa_Yuni->tampilkanUmur() ; 
-$mahasiswa_Faiz->tampilkanAngkatan();
-$mahasiswa_Faiz->tampilkanUmur()  ;
+ </body>
+</html>
